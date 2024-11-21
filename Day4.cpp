d868a096e8f3ac675a70da0245f27ba67fa05c44
Blessings Aadi <aryanpandya2310@outlook.com>
2024-11-21 23:18:20 +0530

    Day 4: Rotate Array

--- Day4.cpp
+++ Day4.cpp
@@ -12,22 +12,6 @@ void rotateArr(vector<int>& arr, int d) {
     reverse(arr.begin(), arr.end());
 }
 
-
-// void rotateArr(vector<int>& arr, int d) {
-//     // code here
-//     for(int i = 0; i < d; i++) {
-//         arr.push_back(arr[i]);
-//     }
-
-//     for(int i = 0; i < arr.size(); i++) {
-//         arr[i] = arr[i+d];
-//     }
-
-//     while(d--) {
-//         arr.pop_back();
-//     }    
-// }
-
 int main() {
     vector<int> arr {3,7,8,9,10,11};
     rotateArr(arr, 2);
